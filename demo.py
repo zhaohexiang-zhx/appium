2bd02e66952bd17edc1c1ee02c8968baa5d4de45
zhaohexiang-zhx <zhx190523@163.com>
2021-03-01 15:46:06 +0800

    加延时

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@
 
 # 导入库
 from appium import webdriver
+import time
 
 caps = {}
 caps["platformName"] = "Android"
@@ -12,19 +13,35 @@ caps["appActivity"] = "com.czy.hiconmultiscreen.mvp.ui.splash.SplashActivity"
 caps["appPackage"] = "com.czy.hiconmultiscreen"
 caps["autoGrantPermissions"] = "true"
 caps["ensureWebviewsHavePages"] = True
+# 不重置app信息
+caps['noReset'] = True
 
 # 初始化实例，调用导入库的方法
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+driver.implicitly_wait(2)
 
 # case
+# 消息
 el2 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.ImageView")
 el2.click()
-el3 = driver.find_element_by_id("com.czy.hiconmultiscreen:id/backImageView")
+time.sleep(3)
+# 密码登录
+# el3 = driver.find_element_by_id("com.czy.hiconmultiscreen:id/login_password")
+# /之前的部分可以去掉
+el3 = driver.find_element_by_id("login_password")
 el3.click()
+time.sleep(2)
+# 返回
+el4 = driver.find_element_by_id("backImageView")
+el4.click()
+time.sleep(2)
+# 返回
+el5 = driver.find_element_by_id("backImageView")
+el5.click()
 
 
 # 断言
 
-driver.quit()
+# driver.quit()
 
 
